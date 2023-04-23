374ae4bd8846b9b754d949c1b3caef17ec001abc
NikolaLoncarski <nidzafrom023@gmail.com>
2023-04-23 14:31:49 +0200

    new branch

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ const getGames = async function () {
   storeData.map((element) => {
     const { storeID, storeName, isActive, images } = element;
     const listItem = document.createElement("li");
+    const name = document.createElement("h2");
 
     listItem.dataset.id = `${storeID}`;
     const storeLogo = document.createElement("img");
     const { logo, banners, icon } = images;
+    name.innerText = `${storeName}`;
     listItem.data = `${storeID}`;
+
     listItem.classList.add("card");
 
     if (isActive === 1) {
       storeLogo.src = `https://www.cheapshark.com${logo}`;
       listItem.appendChild(storeLogo);
       storesNav.appendChild(listItem);
+      listItem.appendChild(name);
     }
   });
 
@@ -46,20 +50,44 @@ const fetchGames = async (id) => {
   console.log(gameData);
   removeAllChildNodes(gameCards);
   gameData.map((ele) => {
-    const { title, dealID, normalPrice, releaseDate, salePrice, savings } = ele;
-
-    // if (gameCards.childNodes === true) {
-    //   gameCards.removeChild(gameCard);
-    // }
-
+    const {
+      title,
+      dealID,
+      normalPrice,
+      releaseDate,
+      salePrice,
+      savings,
+      thumb,
+    } = ele;
+    const unixTime = releaseDate * 1000;
+    const date = new Date(unixTime);
     const gameCard = document.createElement("div");
     const gameTitle = document.createElement("h4");
+    const gameThumb = document.createElement("img");
+    const normPrice = document.createElement("span");
+    const gameReleaseDate = document.createElement("span");
+
+    gameCard.classList.add("game");
+    normPrice.classList.add("normPrice");
+    gameReleaseDate.classList.add("gameDate");
 
     gameCard.dataset.id = `${dealID}`;
     gameTitle.innerText = title;
+    gameThumb.src = `${thumb}`;
+    normPrice.innerText = `${normalPrice}`;
+    if (date > 0) {
+      gameReleaseDate.innerText = `Release Date:${date.toLocaleDateString(
+        "en-US"
+      )}`;
+    } else {
+      gameReleaseDate.innerText = "Release Date:N/A";
+    }
 
     gameCard.appendChild(gameTitle);
     gameCards.appendChild(gameCard);
+    gameCard.prepend(gameThumb);
+    gameCard.append(normPrice);
+    gameCard.append(gameReleaseDate);
   });
 };
 function removeAllChildNodes(parent) {
