ac1811a71c335991225f7b8e6e2a478aaa95be67
NikolaLoncarski <nidzafrom023@gmail.com>
2023-04-23 17:46:13 +0200

    comment

--- index.js
+++ index.js
@@ -2,23 +2,26 @@
 const storesNav = document.querySelector(".stores-Side-Nav");
 const gameCards = document.querySelector(".gameCards");
 
-//--------------
-const getGames = async function () {
+///// Fetches stores
+const fetchStores = async function () {
   const fetchStores = await fetch("https://www.cheapshark.com/api/1.0/stores");
   const storeData = await fetchStores.json();
+
   console.log(storeData);
+
   storeData.map((element) => {
     const { storeID, storeName, isActive, images } = element;
+
     const listItem = document.createElement("li");
-    const name = document.createElement("h2");
-
     listItem.dataset.id = `${storeID}`;
-    const storeLogo = document.createElement("img");
-    const { logo, banners, icon } = images;
-    name.innerText = `${storeName}`;
     listItem.data = `${storeID}`;
-
     listItem.classList.add("card");
+
+    const name = document.createElement("h2");
+    name.innerText = `${storeName}`;
+    const storeLogo = document.createElement("img");
+
+    const { logo, banners, icon } = images;
 
     if (isActive === 1) {
       storeLogo.src = `https://www.cheapshark.com${logo}`;
@@ -28,19 +31,17 @@ const getGames = async function () {
     }
   });
 
-  /////-------------
+  ////makes store cards clicable and starts a api call to get games
   const gameStores = document.querySelectorAll("[data-id]");
-
   gameStores.forEach((ele) => {
     ele.addEventListener("click", () => {
       fetchGames(ele.dataset.id);
     });
   });
 };
-getGames();
+fetchStores();
 
-////--------
-
+///// fethces individual store games
 const fetchGames = async (id) => {
   const fetchGameData = await fetch(
     `https://www.cheapshark.com/api/1.0/deals?storeID=${id}`
@@ -58,23 +59,34 @@ const fetchGames = async (id) => {
       salePrice,
       savings,
       thumb,
+      metacriticLink,
     } = ele;
     const unixTime = releaseDate * 1000;
     const date = new Date(unixTime);
+    ////overall card div
     const gameCard = document.createElement("div");
-    const gameTitle = document.createElement("h4");
-    const gameThumb = document.createElement("img");
-    const normPrice = document.createElement("span");
-    const gameReleaseDate = document.createElement("span");
-
     gameCard.classList.add("game");
+    gameCard.dataset.id = `${dealID}`;
+
+    const gameTitle = document.createElement("h4");
+    gameTitle.innerText = title;
+
+    const gameThumb = document.createElement("img");
+    gameThumb.src = `${thumb}`;
+
+    const normPrice = document.createElement("span");
     normPrice.classList.add("normPrice");
+    normPrice.innerText = `${normalPrice}`;
+
+    const gameReleaseDate = document.createElement("span");
     gameReleaseDate.classList.add("gameDate");
 
-    gameCard.dataset.id = `${dealID}`;
-    gameTitle.innerText = title;
-    gameThumb.src = `${thumb}`;
-    normPrice.innerText = `${normalPrice}`;
+    const gameInfoLink = document.createElement("a");
+    gameInfoLink.href = `https://www.metacritic.com${metacriticLink}`;
+    gameInfoLink.target = "_blank";
+    gameInfoLink.text = "More Info...";
+
+    ///Check if there is a realse date information
     if (date > 0) {
       gameReleaseDate.innerText = `Release Date:${date.toLocaleDateString(
         "en-US"
@@ -88,8 +100,11 @@ const fetchGames = async (id) => {
     gameCard.prepend(gameThumb);
     gameCard.append(normPrice);
     gameCard.append(gameReleaseDate);
+    gameCard.appendChild(gameInfoLink);
   });
 };
+
+/// helper function to clear the whole store api call before a new one is loaded
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
