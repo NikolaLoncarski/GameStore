bd59239cfdad404bd6da6a0a8082498c191ffcf8
NikolaLoncarski <nidzafrom023@gmail.com>
2023-04-23 22:44:09 +0200

    sort

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 "use strict";
+const nav = document.querySelector("nav");
 const storesNav = document.querySelector(".stores-Side-Nav");
 const gameCards = document.querySelector(".gameCards");
-
+const menuButton = document.querySelector(".menuButton");
 ///// Fetches stores
+
+const sortDivs = [];
 const fetchStores = async function () {
   const fetchStores = await fetch("https://www.cheapshark.com/api/1.0/stores");
   const storeData = await fetchStores.json();
@@ -31,7 +34,7 @@ const fetchStores = async function () {
     }
   });
 
-  ////makes store cards clicable and starts a api call to get games
+  ////makes store cards clickable and starts an api-call to get games
   const gameStores = document.querySelectorAll("[data-id]");
   gameStores.forEach((ele) => {
     ele.addEventListener("click", () => {
@@ -63,30 +66,42 @@ const fetchGames = async (id) => {
     } = ele;
     const unixTime = releaseDate * 1000;
     const date = new Date(unixTime);
+
     ////overall card div
     const gameCard = document.createElement("div");
     gameCard.classList.add("game");
     gameCard.dataset.id = `${dealID}`;
 
     const gameTitle = document.createElement("h4");
+    gameTitle.classList.add("gameTitle");
     gameTitle.innerText = title;
 
     const gameThumb = document.createElement("img");
     gameThumb.src = `${thumb}`;
 
+    //--------------Prices ------------//
+    const gamePrices = document.createElement("div");
+    gamePrices.classList.add("gamePrices");
+
     const normPrice = document.createElement("span");
     normPrice.classList.add("normPrice");
-    normPrice.innerText = `${normalPrice}`;
+    normPrice.innerText = `Normal Price:${normalPrice}$`;
 
+    const price = document.createElement("span");
+    price.classList.add("price");
+    price.innerText = `Current Price:${salePrice}$`;
+
+    const moneySaved = document.createElement("span");
+    moneySaved.classList.add("moneySaved");
+    moneySaved.innerText = `Money Saved:${parseFloat(savings).toFixed(1)}%`;
+
+    gamePrices.appendChild(normPrice);
+    gamePrices.appendChild(moneySaved);
+    gamePrices.appendChild(price);
+    ///---------------//-----------------------//
     const gameReleaseDate = document.createElement("span");
     gameReleaseDate.classList.add("gameDate");
 
-    const gameInfoLink = document.createElement("a");
-    gameInfoLink.href = `https://www.metacritic.com${metacriticLink}`;
-    gameInfoLink.target = "_blank";
-    gameInfoLink.text = "More Info...";
-
-    ///Check if there is a realse date information
     if (date > 0) {
       gameReleaseDate.innerText = `Release Date:${date.toLocaleDateString(
         "en-US"
@@ -96,12 +111,32 @@ const fetchGames = async (id) => {
     }
 
     gameCard.appendChild(gameTitle);
-    gameCards.appendChild(gameCard);
     gameCard.prepend(gameThumb);
-    gameCard.append(normPrice);
+    gameCard.append(gamePrices);
     gameCard.append(gameReleaseDate);
-    gameCard.appendChild(gameInfoLink);
+    gameCards.appendChild(gameCard);
+    ///Check if there is a realse date information
+    if (metacriticLink) {
+      const gameInfoLink = document.createElement("a");
+      gameInfoLink.href = `https://www.metacritic.com${metacriticLink}`;
+      gameInfoLink.target = "_blank";
+      gameInfoLink.text = "More Info...";
+      gameCard.appendChild(gameInfoLink);
+    }
+
+    ////--------------sort--------------///
+    const sortPrices = [
+      gameCard.childNodes[2].childNodes[2].textContent.slice(-5, -1),
+    ];
+    sortPrices.forEach((e) => {
+      sortDivs.push(e);
+    });
   });
+  console.log(
+    sortDivs.sort((a, b) => {
+      return a > b;
+    })
+  );
 };
 
 /// helper function to clear the whole store api call before a new one is loaded
@@ -110,3 +145,11 @@ function removeAllChildNodes(parent) {
     parent.removeChild(parent.firstChild);
   }
 }
+
+menuButton.addEventListener("click", () => {
+  if (storesNav.style.display !== "none") {
+    storesNav.style.display = "none";
+  } else {
+    storesNav.style.display = "flex";
+  }
+});
