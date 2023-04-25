17cf64d38868c1ca5f3a286c12a54ff20feaa252
NikolaLoncarski <nidzafrom023@gmail.com>
2023-04-25 22:16:48 +0200

    eureka

---
+++ cart.js
@@ -0,0 +1,2 @@
+let cartData = JSON.parse(localStorage.getItem("gamesInCart"));
+console.log(cartData);
--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const nav = document.querySelector("nav");
 const storesNav = document.querySelector(".stores-Side-Nav");
 const gameCards = document.querySelector(".gameCards");
 const menuButton = document.querySelector(".menuButton");
+let gameInfo = [];
 let cart = [];
+if (!localStorage.getItem("gamesInCart")) {
+  cart = [];
+} else {
+  cart = JSON.parse(localStorage.getItem("gamesInCart"));
+}
+
 ///// Fetches stores
 
 const fetchStores = async function () {
@@ -52,90 +59,8 @@ const fetchGames = async (id) => {
 
   const gameData = await fetchGameData.json();
   console.log(gameData);
-  storeData(gameData);
-  const createCards = (cardElements) => {
-    cardElements.map((ele) => {
-      const {
-        title,
-        dealID,
-        normalPrice,
-        releaseDate,
-        salePrice,
-        savings,
-        thumb,
-        metacriticLink,
-      } = ele;
-
-      const unixTime = releaseDate * 1000;
-      const date = new Date(unixTime);
-
-      ////overall card div
-      const gameCard = document.createElement("div");
-      gameCard.classList.add("game");
-      gameCard.dataset.id = `${dealID}`;
-
-      const gameTitle = document.createElement("h4");
-      gameTitle.classList.add("gameTitle");
-      gameTitle.innerText = title;
-
-      const gameThumb = document.createElement("img");
-      gameThumb.src = `${thumb}`;
-
-      //--------------Prices ------------//
-      const gamePrices = document.createElement("div");
-      gamePrices.classList.add("gamePrices");
-
-      const normPrice = document.createElement("span");
-      normPrice.classList.add("normPrice");
-      normPrice.innerText = `Normal Price:${normalPrice}$`;
-
-      const price = document.createElement("span");
-      price.classList.add("price");
-      price.innerText = `Current Price:${salePrice}$`;
-
-      const moneySaved = document.createElement("span");
-      moneySaved.classList.add("moneySaved");
-      moneySaved.innerText = `Money Saved:${parseFloat(savings).toFixed(1)}%`;
-
-      const saveButton = document.createElement("button");
-      saveButton.classList.add("saveButton");
-      saveButton.innerText = "Save to Cart";
-
-      gamePrices.appendChild(normPrice);
-      gamePrices.appendChild(moneySaved);
-      gamePrices.appendChild(price);
-      ///---------------//-----------------------//
-      const gameReleaseDate = document.createElement("span");
-      gameReleaseDate.classList.add("gameDate");
-
-      if (date > 0) {
-        gameReleaseDate.innerText = `Release Date:${date.toLocaleDateString(
-          "en-US"
-        )}`;
-      } else {
-        gameReleaseDate.innerText = "Release Date:N/A";
-      }
-
-      gameCard.appendChild(gameTitle);
-      gameCard.prepend(gameThumb);
-      gameCard.append(gamePrices);
-      gameCard.append(gameReleaseDate);
-      gameCard.append(saveButton);
-      gameCards.appendChild(gameCard);
-      ///Check if there is a realse date information
-      if (metacriticLink) {
-        const gameInfoLink = document.createElement("a");
-        gameInfoLink.href = `https://www.metacritic.com${metacriticLink}`;
-        gameInfoLink.target = "_blank";
-        gameInfoLink.text = "More Info...";
-        gameCard.appendChild(gameInfoLink);
-      }
-      saveButton.addEventListener("click", () => {
-        storeData(ele);
-      });
-    });
-  };
-  createCards(gameData);
+  gamesData(gameData);
+  createCards(gameInfo[0]);
 };
 
 /// helper function to clear the whole store api call before a new one is loaded
@@ -152,7 +77,104 @@ menuButton.addEventListener("click", () => {
     storesNav.style.display = "flex";
   }
 });
-function storeData(data) {
-  cart.push(data);
-  console.log(cart);
+
+////------stores data in an array outside the api call--------/////////
+function gamesData(games) {
+  gameInfo.push(games);
 }
+function cartGames(games) {
+  cart.push(games);
+}
+
+////--------creates new dom elements from the new array-------///////
+const createCards = (cardElements) => {
+  cardElements.map((ele) => {
+    const {
+      title,
+      dealID,
+      normalPrice,
+      releaseDate,
+      salePrice,
+      savings,
+      thumb,
+      metacriticLink,
+    } = ele;
+
+    const unixTime = releaseDate * 1000;
+    const date = new Date(unixTime);
+
+    ////overall card div
+    const gameCard = document.createElement("div");
+    gameCard.classList.add("game");
+    gameCard.dataset.id = `${dealID}`;
+
+    const gameTitle = document.createElement("h4");
+    gameTitle.classList.add("gameTitle");
+    gameTitle.innerText = title;
+
+    const gameThumb = document.createElement("img");
+    gameThumb.src = `${thumb}`;
+
+    //--------------Prices ------------//
+    const gamePrices = document.createElement("div");
+    gamePrices.classList.add("gamePrices");
+
+    const normPrice = document.createElement("span");
+    normPrice.classList.add("normPrice");
+    normPrice.innerText = `Normal Price:${normalPrice}$`;
+
+    const price = document.createElement("span");
+    price.classList.add("price");
+    price.innerText = `Current Price:${salePrice}$`;
+
+    const moneySaved = document.createElement("span");
+    moneySaved.classList.add("moneySaved");
+    moneySaved.innerText = `Money Saved:${parseFloat(savings).toFixed(1)}%`;
+
+    const saveButton = document.createElement("button");
+    saveButton.classList.add("saveButton");
+    saveButton.innerText = "Save to Cart";
+
+    gamePrices.appendChild(normPrice);
+    gamePrices.appendChild(moneySaved);
+    gamePrices.appendChild(price);
+    ///---------------//-----------------------//
+    const gameReleaseDate = document.createElement("span");
+    gameReleaseDate.classList.add("gameDate");
+
+    if (date > 0) {
+      gameReleaseDate.innerText = `Release Date:${date.toLocaleDateString(
+        "en-US"
+      )}`;
+    } else {
+      gameReleaseDate.innerText = "Release Date:N/A";
+    }
+
+    gameCard.appendChild(gameTitle);
+    gameCard.prepend(gameThumb);
+    gameCard.append(gamePrices);
+    gameCard.append(gameReleaseDate);
+    gameCard.append(saveButton);
+    gameCards.appendChild(gameCard);
+    ///Check if there is a realse date information
+    if (metacriticLink) {
+      const gameInfoLink = document.createElement("a");
+      gameInfoLink.href = `https://www.metacritic.com${metacriticLink}`;
+      gameInfoLink.target = "_blank";
+      gameInfoLink.text = "More Info...";
+      gameCard.appendChild(gameInfoLink);
+    }
+    saveButton.addEventListener("click", () => {
+      // cartGames(ele);
+      // console.log(cart);
+      cart.push(ele);
+      let existingData = localStorage.getItem("gamesInCart");
+      existingData = existingData ? existingData.split(",") : [];
+      existingData.push(ele);
+      // console.log(JSON.parse(localStorage.getItem("gamesInCart")));
+      // cart.push(JSON.parse(localStorage.getItem("gamesInCart")));
+      localStorage.gamesInCart = JSON.stringify(cart);
+      // localStorage.setItem("gamesInCart", JSON.stringify(...cart));
+    });
+  });
+};
