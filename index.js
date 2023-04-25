430d65b0427b89d93c6c071b8cbf4f4135e2a5bf
NikolaLoncarski <nidzafrom023@gmail.com>
2023-04-25 21:00:20 +0200

    hope

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const nav = document.querySelector("nav");
 const storesNav = document.querySelector(".stores-Side-Nav");
 const gameCards = document.querySelector(".gameCards");
 const menuButton = document.querySelector(".menuButton");
+let cart = [];
 ///// Fetches stores
 
-const sortDivs = [];
 const fetchStores = async function () {
   const fetchStores = await fetch("https://www.cheapshark.com/api/1.0/stores");
   const storeData = await fetchStores.json();
@@ -52,91 +52,90 @@ const fetchGames = async (id) => {
 
   const gameData = await fetchGameData.json();
   console.log(gameData);
-  removeAllChildNodes(gameCards);
-  gameData.map((ele) => {
-    const {
-      title,
-      dealID,
-      normalPrice,
-      releaseDate,
-      salePrice,
-      savings,
-      thumb,
-      metacriticLink,
-    } = ele;
-    const unixTime = releaseDate * 1000;
-    const date = new Date(unixTime);
+  storeData(gameData);
+  const createCards = (cardElements) => {
+    cardElements.map((ele) => {
+      const {
+        title,
+        dealID,
+        normalPrice,
+        releaseDate,
+        salePrice,
+        savings,
+        thumb,
+        metacriticLink,
+      } = ele;
 
-    ////overall card div
-    const gameCard = document.createElement("div");
-    gameCard.classList.add("game");
-    gameCard.dataset.id = `${dealID}`;
+      const unixTime = releaseDate * 1000;
+      const date = new Date(unixTime);
 
-    const gameTitle = document.createElement("h4");
-    gameTitle.classList.add("gameTitle");
-    gameTitle.innerText = title;
+      ////overall card div
+      const gameCard = document.createElement("div");
+      gameCard.classList.add("game");
+      gameCard.dataset.id = `${dealID}`;
 
-    const gameThumb = document.createElement("img");
-    gameThumb.src = `${thumb}`;
+      const gameTitle = document.createElement("h4");
+      gameTitle.classList.add("gameTitle");
+      gameTitle.innerText = title;
 
-    //--------------Prices ------------//
-    const gamePrices = document.createElement("div");
-    gamePrices.classList.add("gamePrices");
+      const gameThumb = document.createElement("img");
+      gameThumb.src = `${thumb}`;
 
-    const normPrice = document.createElement("span");
-    normPrice.classList.add("normPrice");
-    normPrice.innerText = `Normal Price:${normalPrice}$`;
+      //--------------Prices ------------//
+      const gamePrices = document.createElement("div");
+      gamePrices.classList.add("gamePrices");
 
-    const price = document.createElement("span");
-    price.classList.add("price");
-    price.innerText = `Current Price:${salePrice}$`;
+      const normPrice = document.createElement("span");
+      normPrice.classList.add("normPrice");
+      normPrice.innerText = `Normal Price:${normalPrice}$`;
 
-    const moneySaved = document.createElement("span");
-    moneySaved.classList.add("moneySaved");
-    moneySaved.innerText = `Money Saved:${parseFloat(savings).toFixed(1)}%`;
+      const price = document.createElement("span");
+      price.classList.add("price");
+      price.innerText = `Current Price:${salePrice}$`;
 
-    gamePrices.appendChild(normPrice);
-    gamePrices.appendChild(moneySaved);
-    gamePrices.appendChild(price);
-    ///---------------//-----------------------//
-    const gameReleaseDate = document.createElement("span");
-    gameReleaseDate.classList.add("gameDate");
+      const moneySaved = document.createElement("span");
+      moneySaved.classList.add("moneySaved");
+      moneySaved.innerText = `Money Saved:${parseFloat(savings).toFixed(1)}%`;
 
-    if (date > 0) {
-      gameReleaseDate.innerText = `Release Date:${date.toLocaleDateString(
-        "en-US"
-      )}`;
-    } else {
-      gameReleaseDate.innerText = "Release Date:N/A";
-    }
+      const saveButton = document.createElement("button");
+      saveButton.classList.add("saveButton");
+      saveButton.innerText = "Save to Cart";
 
-    gameCard.appendChild(gameTitle);
-    gameCard.prepend(gameThumb);
-    gameCard.append(gamePrices);
-    gameCard.append(gameReleaseDate);
-    gameCards.appendChild(gameCard);
-    ///Check if there is a realse date information
-    if (metacriticLink) {
-      const gameInfoLink = document.createElement("a");
-      gameInfoLink.href = `https://www.metacritic.com${metacriticLink}`;
-      gameInfoLink.target = "_blank";
-      gameInfoLink.text = "More Info...";
-      gameCard.appendChild(gameInfoLink);
-    }
+      gamePrices.appendChild(normPrice);
+      gamePrices.appendChild(moneySaved);
+      gamePrices.appendChild(price);
+      ///---------------//-----------------------//
+      const gameReleaseDate = document.createElement("span");
+      gameReleaseDate.classList.add("gameDate");
 
-    ////--------------sort--------------///
-    const sortPrices = [
-      gameCard.childNodes[2].childNodes[2].textContent.slice(-5, -1),
-    ];
-    sortPrices.forEach((e) => {
-      sortDivs.push(e);
+      if (date > 0) {
+        gameReleaseDate.innerText = `Release Date:${date.toLocaleDateString(
+          "en-US"
+        )}`;
+      } else {
+        gameReleaseDate.innerText = "Release Date:N/A";
+      }
+
+      gameCard.appendChild(gameTitle);
+      gameCard.prepend(gameThumb);
+      gameCard.append(gamePrices);
+      gameCard.append(gameReleaseDate);
+      gameCard.append(saveButton);
+      gameCards.appendChild(gameCard);
+      ///Check if there is a realse date information
+      if (metacriticLink) {
+        const gameInfoLink = document.createElement("a");
+        gameInfoLink.href = `https://www.metacritic.com${metacriticLink}`;
+        gameInfoLink.target = "_blank";
+        gameInfoLink.text = "More Info...";
+        gameCard.appendChild(gameInfoLink);
+      }
+      saveButton.addEventListener("click", () => {
+        storeData(ele);
+      });
     });
-  });
-  console.log(
-    sortDivs.sort((a, b) => {
-      return a > b;
-    })
-  );
+  };
+  createCards(gameData);
 };
 
 /// helper function to clear the whole store api call before a new one is loaded
@@ -153,3 +152,7 @@ menuButton.addEventListener("click", () => {
     storesNav.style.display = "flex";
   }
 });
+function storeData(data) {
+  cart.push(data);
+  console.log(cart);
+}
